4ca8dfb3af7865c9a38be1ba1127042cfb653032
jiafuming <workjfm142837@163.com>
2021-01-19 14:42:00 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入字体图标样式
+import './assets/fonts/iconfont.css'
+/* 导入全局样式表 */
+import './assets/css/global.css'
 
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://www.test.com/api/admin/'
 Vue.config.productionTip = false
 
 new Vue({
